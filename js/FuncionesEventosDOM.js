177838fb61d15131f25954a69151d2d8f3c09fa1
ExequielMontero <exeee2015@gmail.com>
2025-03-13 17:25:39 -0300

    Inicio de los ejercicios

---
+++ js/FuncionesEventosDOM.js
@@ -0,0 +1,12 @@
+const $nav = document.createElement("nav"),
+  links = [
+    { title: "seccion 1", src: "index.html/seccion1" },
+    { title: "seccion 2", src: "index.html/seccion2" },
+    { title: "seccion 3", src: "index.html/seccion3" },
+    { title: "seccion 4", src: "index.html/seccion4" },
+    { title: "seccion 5", src: "index.html/seccion5" },
+  ];
+
+$nav.classList.add("");
+
+export const navLinks = function (e) {};
